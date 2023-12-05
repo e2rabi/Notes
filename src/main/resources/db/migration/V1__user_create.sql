886573619208ef51b521f04dbefe9986821795fc
errabi ayoub <errabi.ayoub@gmail.com>
2023-12-06 00:44:11 +0100

    Setup flyway for database migration

---
+++ src/main/resources/db/migration/V1__user_create.sql
@@ -0,0 +1,9 @@
+  drop table if exists users cascade ;
+      create table users (
+          user_id uuid not null,
+          email varchar(255),
+          password varchar(255),
+          profile_picture varchar(255),
+          username varchar(255),
+          primary key (user_id)
+);
