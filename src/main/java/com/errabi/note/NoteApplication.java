9e6dbce8d9d774a1d15151c24a8ff4d4944c143d
errabi ayoub <errabi.ayoub@gmail.com>
2023-12-07 23:29:34 +0100

    Setup Hikari connection pool
    (cherry picked from commit 960f95bb41443c934cccb0d9508407b20db87c84)

--- src/main/java/com/errabi/note/NoteApplication.java
+++ src/main/java/com/errabi/note/NoteApplication.java
@@ -1,15 +1,21 @@
 package com.errabi.note;
 
 import com.errabi.note.entities.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @SpringBootApplication
 public class NoteApplication implements CommandLineRunner {
      @Autowired
 	UserRepository userRepository ;
+	@Autowired
+	private DataSource dataSource;
 	public static void main(String[] args) {
 		SpringApplication.run(NoteApplication.class, args);
 	}
@@ -18,7 +24,7 @@ public class NoteApplication implements CommandLineRunner {
 	public void run(String... args)  {
 		User user = new User();
 		user.setUsername("Ayoub");
-
+        log.info("Datasource : {}",dataSource.getClass().getName());
 		userRepository.save(user);
 	}
 }
