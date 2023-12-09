63b80e0a66914e409e3ade39927f70655106a271
errabi ayoub <errabi.ayoub@gmail.com>
2023-12-09 18:26:08 +0100

    Setup Sequence for User primary key
    (cherry picked from commit 56891b4c518db240e13d1187ba408752e4ee047e)

--- src/main/java/com/errabi/note/entities/User.java
+++ src/main/java/com/errabi/note/entities/User.java
@@ -3,15 +3,13 @@ package com.errabi.note.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID userId;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long userId;
     private String username;
     private String email;
     private String password;
---
+++ src/main/resources/db/migration/V0_sequences.sql
@@ -0,0 +1,2 @@
+  drop sequence if exists users_seq ;
+  create sequence users_seq start with 1 increment by 1 ;
