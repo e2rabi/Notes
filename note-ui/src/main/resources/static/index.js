f066bd15d87321827eb21ca5ee0708892578a98a
errabi ayoub <errabi.ayoub@gmail.com>
2024-06-01 13:18:08 +0100

    add index.js to optimize js module import

---
+++ note-ui/src/main/resources/static/index.js
@@ -0,0 +1,8 @@
+import './components/card/Card.js'
+import './components/cardEdit/CardEdit.js'
+import './components/cardPage/CardPage.js'
+import './components/colorPicker/ColorPicker.js'
+import './components/layout/Header.js'
+import './components/layout/NavBar.js'
+import './components/trash/Trash.js'
+import './app.js'
