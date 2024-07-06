46fd58f8fbd0f2a861ba0af8a54b5039529fce45
errabi ayoub <errabi.ayoub@gmail.com>
2024-07-06 16:52:41 +0100

    finish redux setup

--- note-ui/src/main/resources/static/app.js
+++ note-ui/src/main/resources/static/app.js
@@ -9,8 +9,8 @@ app.deletedNotes = [];
 
 // Setup redux store
 const ADD_NOTE = "ADD_NOTE";
-const addNote = (payload) =>
-    ({ type: ADD_NOTE, payload: { ...payload } });
+const addNote = (obj) =>
+    ({ type: ADD_NOTE, payload: obj });
 
 const initialState = {
     notes: [
@@ -28,84 +28,14 @@ const initialState = {
 };
 const reducer = (state = initialState, action) => {
     if (action.type === ADD_NOTE) {
-        const array = [];
-        array.push([action.payload])
-        return array /* {
+        return {
             ...state,
-            notes: [action.payload],
-        }; */
+            notes: [...state.notes, action.payload],
+        };
     }
 };
 const store = Redux.createStore(reducer, initialState);
 app.store = store;
-app.store.dispatch(addNote(
-    [
-        {
-            "id": 2,
-            "name": "Simple note 2",
-            "description": "Note 2 description",
-            "isFavorit": "true",
-            "color": "blue",
-            "pinned": "true",
-            "isRemoved": "false"
-        },
-        {
-            "id": 3,
-            "name": "Simple note 3",
-            "description": "Note 3 description",
-            "isFavorit": "false",
-            "color": "orange",
-            "pinned": "false",
-            "isRemoved": "false"
-        },
-        {
-            "id": 4,
-            "name": "Simple note 4",
-            "description": "Note 4 description",
-            "isFavorit": "false",
-            "color": "dark-green",
-            "pinned": "false",
-            "isRemoved": "false"
-        },
-        {
-            "id": 5,
-            "name": "Simple note 5",
-            "description": "Note 5 description",
-            "isFavorit": "false",
-            "color": "pink",
-            "pinned": "false",
-            "isRemoved": "false"
-        },
-        {
-            "id": 6,
-            "name": "Simple note 6",
-            "description": "Note 6 description",
-            "isFavorit": "false",
-            "color": "brown",
-            "pinned": "false",
-            "isRemoved": "false"
-        },
-        {
-            "id": 7,
-            "name": "Simple note 7",
-            "description": "Note 7 description",
-            "isFavorit": "false",
-            "color": "light-green",
-            "pinned": "false",
-            "isRemoved": "false"
-        },
-        {
-            "id": 8,
-            "name": "Simple note 8",
-            "description": "note 8 description",
-            "isFavorit": "false",
-            "color": "red",
-            "pinned": "true",
-            "status": "F",
-            "isRemoved": "false"
-        }]
-));
-console.log(app.store.getState());
 
 
 // wait for this event to manipulate the DOM
@@ -114,6 +44,10 @@ window.addEventListener("DOMContentLoaded", async () => {
     app.router.init();
     await loadNData();
     await loadDeletedNotes();
+    app.notes.forEach(e => {
+        app.store.dispatch(addNote(e));
+    })
+    console.log(app.store.getState().notes);
 })
 // shorthand methods
 const $ = (selector) => document.querySelector(selector);
